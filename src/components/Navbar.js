4d99e7cb61317bf2217fe0959c6652f14e43073d
Hairun Huang <hairunhuang@gmail.com>
2024-05-18 20:56:20 -0700

    nav rm borderb

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ async function Navbar() {
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-sm transition-all">
             <MaxWidthWrapper>
-                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+                <div className='flex h-14 items-center justify-between'>
                     <Link href='/' className='flex z-40 font-semibold text-lg'>
                         <span>MyProduct</span>
                     </Link>
