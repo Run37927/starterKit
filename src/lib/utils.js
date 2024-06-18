b569e6c6c9861fa50ed8475c71516763c0e5859d
Hairun Huang <hairunhuang@gmail.com>
2024-06-17 17:21:01 -0700

    format price fn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const formatPrice = (price) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "CAD",
+  });
+
+  return formatter.format(price);
+};
