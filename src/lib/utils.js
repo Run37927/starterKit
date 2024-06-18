ac7c84378ba3eda5ed7de99b63139709933d5f53
Hairun Huang <hairunhuang@gmail.com>
2024-06-17 17:26:55 -0700

    construct metadata fn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,3 +13,31 @@ export const formatPrice = (price) => {
 
   return formatter.format(price);
 };
+
+
+// TODO: update this
+export function constructMetadata({
+  title = "",
+  description = "",
+  image = "/",
+  icons = "/favicon.ico",
+} = {}) {
+  return {
+    title,
+    description,
+    icons,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@hairunhuang",
+    },
+    metadataBase: new URL('')
+  };
+}
