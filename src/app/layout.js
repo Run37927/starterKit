fea4facbfa61a56df63cc5a1ea468c8b7e48c796
Hairun Huang <hairunhuang@gmail.com>
2024-07-30 22:04:26 -0700

    update package

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import { cn, constructMetadata } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
 import { Toaster } from '@/components/ui/toaster';
 import Providers from '@/components/Providers';
-import { Analytics } from "@vercel/analytics/react"
 
 const mulish = Mulish({ subsets: ['latin'] });
 
@@ -18,7 +17,6 @@ export default function RootLayout({ children }) {
           <Toaster />
           <Navbar />
           {children}
-          <Analytics />
         </Providers>
       </body>
     </html>
